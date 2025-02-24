a046ff0757e27f75fb253fc7e1e3bcad8071e7ae
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2025-02-24 07:57:06 -0600

    Create Buttons.cs

---
+++ Template/Buttons.cs
@@ -0,0 +1,74 @@
+public static class Buttons
+{
+    public static string button1_name = "Button 1";
+    public static void Button1()
+    {
+        /* Functionality */
+    }
+
+    public static string button2_name = "Button 2";
+    public static void Button2()
+    {
+        /* Functionality */
+    }
+
+    public static string button3_name = "Button 3";
+    public static void Button3()
+    {
+        /* Functionality */
+    }
+
+    public static string button4_name = "Button 4";
+    public static void Button4()
+    {
+        /* Functionality */
+    }
+
+    public static string button5_name = "Button 5";
+    public static void Button5()
+    {
+        /* Functionality */
+    }
+
+    public static string button6_name = "Button 6";
+    public static void Button6()
+    {
+        /* Functionality */
+    }
+
+    public static string button7_name = "Button 7";
+    public static void Button7()
+    {
+        /* Functionality */
+    }
+
+    public static string button8_name = "Button 8";
+    public static void Button8()
+    {
+        /* Functionality */
+    }
+
+    public static string button9_name = "Button 9";
+    public static void Button9()
+    {
+        /* Functionality */
+    }
+
+    public static string button10_name = "Button 10";
+    public static void Button10()
+    {
+        /* Functionality */
+    }
+
+    public static string button11_name = "Button 11";
+    public static void Button11()
+    {
+        /* Functionality */
+    }
+
+    public static string button12_name = "Button 12";
+    public static void Button12()
+    {
+        /* Functionality */
+    }
+}
