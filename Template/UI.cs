a8b0890e3050691c4e70a1e6bd6e9ee85f67a49f
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2025-02-24 07:57:42 -0600

    Update UI.cs

--- Template/UI.cs
+++ Template/UI.cs
@@ -1,7 +1,7 @@
 using BepInEx;
 using UnityEngine;
 
-namespace OptimizeX
+namespace UI
 {
     [BepInPlugin("azure.mod.template", "azure.mod.template", "1.0.0")]
     public class GUI : BaseUnityPlugin
@@ -10,64 +10,64 @@ namespace OptimizeX
         {
             UnityEngine.GUI.color = Color.black;
 
-            if (UnityEngine.GUI.Button(new Rect(0f, 0f, 280f, 35f), Mods.button1_name))
+            if (UnityEngine.GUI.Button(new Rect(0f, 0f, 280f, 35f), Buttons.button1_name))
             {
-                Mods.Button1();
+                Buttons.Button1();
             }
 
-            if (UnityEngine.GUI.Button(new Rect(0f, 40f, 280f, 35f), Mods.button2_name))
+            if (UnityEngine.GUI.Button(new Rect(0f, 40f, 280f, 35f), Buttons.button2_name))
             {
-                Mods.Button2();
+                Buttons.Button2();
             }
 
-            if (UnityEngine.GUI.Button(new Rect(0f, 80f, 280f, 35f), Mods.button3_name))
+            if (UnityEngine.GUI.Button(new Rect(0f, 80f, 280f, 35f), Buttons.button3_name))
             {
-                Mods.Button3();
+                Buttons.Button3();
             }
 
-            if (UnityEngine.GUI.Button(new Rect(0f, 120f, 280f, 35f), Mods.button4_name))
+            if (UnityEngine.GUI.Button(new Rect(0f, 120f, 280f, 35f), Buttons.button4_name))
             {
-                Mods.Button4();
+                Buttons.Button4();
             }
 
-            if (UnityEngine.GUI.Button(new Rect(0f, 160f, 280f, 35f), Mods.button5_name))
+            if (UnityEngine.GUI.Button(new Rect(0f, 160f, 280f, 35f), Buttons.button5_name))
             {
-                Mods.Button5();
+                Buttons.Button5();
             }
 
-            if (UnityEngine.GUI.Button(new Rect(0f, 200f, 280f, 35f), Mods.button6_name))
+            if (UnityEngine.GUI.Button(new Rect(0f, 200f, 280f, 35f), Buttons.button6_name))
             {
-                Mods.Button6();
+                Buttons.Button6();
             }
 
-            if (UnityEngine.GUI.Button(new Rect(0f, 240f, 280f, 35f), Mods.button7_name))
+            if (UnityEngine.GUI.Button(new Rect(0f, 240f, 280f, 35f), Buttons.button7_name))
             {
-                Mods.Button7();
+                Buttons.Button7();
             }
 
-            if (UnityEngine.GUI.Button(new Rect(0f, 280f, 280f, 35f), Mods.button8_name))
+            if (UnityEngine.GUI.Button(new Rect(0f, 280f, 280f, 35f), Buttons.button8_name))
             {
-                Mods.Button8();
+                Buttons.Button8();
             }
 
-            if (UnityEngine.GUI.Button(new Rect(0f, 320f, 280f, 35f), Mods.button9_name))
+            if (UnityEngine.GUI.Button(new Rect(0f, 320f, 280f, 35f), Buttons.button9_name))
             {
-                Mods.Button9();
+                Buttons.Button9();
             }
 
-            if (UnityEngine.GUI.Button(new Rect(0f, 360f, 280f, 35f), Mods.button10_name))
+            if (UnityEngine.GUI.Button(new Rect(0f, 360f, 280f, 35f), Buttons.button10_name))
             {
-                Mods.Button10();
+                Buttons.Button10();
             }
 
-            if (UnityEngine.GUI.Button(new Rect(0f, 400f, 280f, 35f), Mods.button11_name))
+            if (UnityEngine.GUI.Button(new Rect(0f, 400f, 280f, 35f), Buttons.button11_name))
             {
-                Mods.Button11();
+                Buttons.Button11();
             }
 
-            if (UnityEngine.GUI.Button(new Rect(0f, 440f, 280f, 35f), Mods.button12_name))
+            if (UnityEngine.GUI.Button(new Rect(0f, 440f, 280f, 35f), Buttons.button12_name))
             {
-                Mods.Button12();
+                Buttons.Button12();
             }
         }
     }
